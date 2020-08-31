6d118ba6f5e2af6b60014fdbf3e9026bc1425218
narcoticfresh <dn@xbe.ch>
2020-08-31 14:25:44 +0200

    start expanding in this test bundle

---
+++ src/GravitonTestServicesBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Graviton\TestServicesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://swisscom.ch
+ */
+class GravitonTestServicesBundle extends Bundle
+{
+}
